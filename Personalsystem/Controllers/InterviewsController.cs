887a7326d71bdd6427f199f9f8601fe515f6b01e
izzcan <cancigerizzet@hotmail.com>
2015-09-29 14:25:14 +0200

    Controllers

---
+++ Personalsystem/Controllers/InterviewsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Personalsystem.Models;
+
+namespace Personalsystem.Controllers
+{
+    public class InterviewsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Interviews
+        public ActionResult Index()
+        {
+            var interviews = db.Interviews.Include(i => i.Applicant).Include(i => i.Interviewer);
+            return View(interviews.ToList());
+        }
+
+        // GET: Interviews/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Interview interview = db.Interviews.Find(id);
+            if (interview == null)
+            {
+                return HttpNotFound();
+            }
+            return View(interview);
+        }
+
+        // GET: Interviews/Create
+        public ActionResult Create()
+        {
+            ViewBag.ApplicantId = new SelectList(db.Users, "Id", "Email");
+            ViewBag.InterviewerId = new SelectList(db.Users, "Id", "Email");
+            return View();
+        }
+
+        // POST: Interviews/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Description,ApplicantId,InterviewerId")] Interview interview)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Interviews.Add(interview);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ApplicantId = new SelectList(db.Users, "Id", "Email", interview.ApplicantId);
+            ViewBag.InterviewerId = new SelectList(db.Users, "Id", "Email", interview.InterviewerId);
+            return View(interview);
+        }
+
+        // GET: Interviews/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Interview interview = db.Interviews.Find(id);
+            if (interview == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ApplicantId = new SelectList(db.Users, "Id", "Email", interview.ApplicantId);
+            ViewBag.InterviewerId = new SelectList(db.Users, "Id", "Email", interview.InterviewerId);
+            return View(interview);
+        }
+
+        // POST: Interviews/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Description,ApplicantId,InterviewerId")] Interview interview)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(interview).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ApplicantId = new SelectList(db.Users, "Id", "Email", interview.ApplicantId);
+            ViewBag.InterviewerId = new SelectList(db.Users, "Id", "Email", interview.InterviewerId);
+            return View(interview);
+        }
+
+        // GET: Interviews/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Interview interview = db.Interviews.Find(id);
+            if (interview == null)
+            {
+                return HttpNotFound();
+            }
+            return View(interview);
+        }
+
+        // POST: Interviews/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Interview interview = db.Interviews.Find(id);
+            db.Interviews.Remove(interview);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Personalsystem/Controllers/VacanciesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Personalsystem.Models;
+
+namespace Personalsystem.Controllers
+{
+    public class VacanciesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Vacancies
+        public ActionResult Index()
+        {
+            var vacancies = db.Vacancies.Include(v => v.Company).Include(v => v.Creator);
+            return View(vacancies.ToList());
+        }
+
+        // GET: Vacancies/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Vacancy vacancy = db.Vacancies.Find(id);
+            if (vacancy == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vacancy);
+        }
+
+        // GET: Vacancies/Create
+        public ActionResult Create()
+        {
+            ViewBag.CompanyId = new SelectList(db.Companies, "Id", "Name");
+            ViewBag.CreatorId = new SelectList(db.Users, "Id", "Email");
+            return View();
+        }
+
+        // POST: Vacancies/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Title,Content,Created,CompanyId,CreatorId")] Vacancy vacancy)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Vacancies.Add(vacancy);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CompanyId = new SelectList(db.Companies, "Id", "Name", vacancy.CompanyId);
+            ViewBag.CreatorId = new SelectList(db.Users, "Id", "Email", vacancy.CreatorId);
+            return View(vacancy);
+        }
+
+        // GET: Vacancies/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Vacancy vacancy = db.Vacancies.Find(id);
+            if (vacancy == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CompanyId = new SelectList(db.Companies, "Id", "Name", vacancy.CompanyId);
+            ViewBag.CreatorId = new SelectList(db.Users, "Id", "Email", vacancy.CreatorId);
+            return View(vacancy);
+        }
+
+        // POST: Vacancies/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Title,Content,Created,CompanyId,CreatorId")] Vacancy vacancy)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(vacancy).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CompanyId = new SelectList(db.Companies, "Id", "Name", vacancy.CompanyId);
+            ViewBag.CreatorId = new SelectList(db.Users, "Id", "Email", vacancy.CreatorId);
+            return View(vacancy);
+        }
+
+        // GET: Vacancies/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Vacancy vacancy = db.Vacancies.Find(id);
+            if (vacancy == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vacancy);
+        }
+
+        // POST: Vacancies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Vacancy vacancy = db.Vacancies.Find(id);
+            db.Vacancies.Remove(vacancy);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
