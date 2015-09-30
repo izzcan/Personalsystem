ed72cb283b3bb5b62162c6848bc65a2caa1caec8
izzcan <cancigerizzet@hotmail.com>
2015-09-30 16:01:28 +0200

    app

--- Personalsystem/Controllers/ApplicationsController.cs
+++ Personalsystem/Controllers/ApplicationsController.cs
@@ -40,7 +40,9 @@ namespace Personalsystem.Controllers
         public ActionResult Create(int? id)
         {
             ViewBag.ApplicantId = new SelectList(db.Users, "Id", "Email");
-            ViewBag.VacancyId = new SelectList(db.Vacancies, "Id", "Title", id);
+            //ViewBag.VacancyId = new SelectList(db.Vacancies, "Id", "Title", id);
+            ViewBag.VacancyId = id;
+            ViewBag.VacancyTitle = db.Vacancies.Find(id).Title;
             return View();
         }
 
