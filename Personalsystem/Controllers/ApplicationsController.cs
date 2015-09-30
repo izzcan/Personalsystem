9d672e7c43b0c156bcf7679fef820ba62ffe3d98
izzcan <cancigerizzet@hotmail.com>
2015-09-30 15:10:46 +0200

    AnsökLänk

--- Personalsystem/Controllers/ApplicationsController.cs
+++ Personalsystem/Controllers/ApplicationsController.cs
@@ -37,10 +37,10 @@ namespace Personalsystem.Controllers
         }
 
         // GET: Applications/Create
-        public ActionResult Create()
+        public ActionResult Create(int? id)
         {
             ViewBag.ApplicantId = new SelectList(db.Users, "Id", "Email");
-            ViewBag.VacancyId = new SelectList(db.Vacancies, "Id", "Title");
+            ViewBag.VacancyId = new SelectList(db.Vacancies, "Id", "Title", id);
             return View();
         }
 
