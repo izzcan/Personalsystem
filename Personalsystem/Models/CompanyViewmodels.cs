786e70e8d556f3cc7d1ca1c35cb1abba81f08c88
Ambrason <ambrason@gmail.com>
2015-10-02 14:29:53 +0200

    Companies cleanup 2

--- Personalsystem/Models/CompanyViewmodels.cs
+++ Personalsystem/Models/CompanyViewmodels.cs
@@ -5,40 +5,6 @@ using System.Web;
 
 namespace Personalsystem.Models
 {
-    ////Ta bort?
-    //public class CompanyProfileViewmodel
-    //{
-    //    public int Id { get; set; }
-    //    public string Name { get; set; }
-
-    //    public ICollection<DepartmentListitemViewmodel> Departments { get; set; }
-    //    public ICollection<NewsListitemViewmodel> NewsItems { get; set; }
-
-    //    public CompanyProfileViewmodel()
-    //    {
-    //        Departments = new List<DepartmentListitemViewmodel>();
-    //        NewsItems = new List<NewsListitemViewmodel>();
-    //    }
-
-    //    public CompanyProfileViewmodel(Company that)
-    //    {
-    //        this.Id = that.Id;
-    //        this.Name = that.Name;
-    //        this.Departments = that.Departments.Select(q => new DepartmentListitemViewmodel() { Id = q.Id, Name = q.Name }).ToList();
-    //        this.NewsItems = that.NewsItems.OrderByDescending(q => q.Created).Take(6).Select(q => new NewsListitemViewmodel() { Id = q.Id, Title = q.Title, Content = q.Content, Created = q.Created, CreatorName = q.Creator.UserName }).ToList();
-    //    }
-    //}
-
-    ////Ta bort?
-    //public class DepartmentListitemViewmodel
-    //{
-    //    public int Id { get; set; }
-    //    public string Name { get; set; }
-    //}
-
-
-
-
     public class CompanyDetailsViewmodel
     {
         public int Id { get; set; }
@@ -49,11 +15,7 @@ namespace Personalsystem.Models
         public ICollection<CompanyDepartmentListitemViewmodel> Departments { get; set; }
         public ICollection<NewsListitemViewmodel> NewsItems { get; set; }
 
-        public CompanyDetailsViewmodel()
-        {
-            //Admins = new List<UserListitemViewmodel>();
-            //Departments = new List<CompanyDepartmentListitemViewmodel>();
-        }
+        public CompanyDetailsViewmodel() { }
 
         public CompanyDetailsViewmodel(Company that)
         {
