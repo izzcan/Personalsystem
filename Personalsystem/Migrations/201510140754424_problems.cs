6669b9c86701e514da68baab5fcaebcea8a8d3b2
Ambrason <ambrason@gmail.com>
2015-10-14 09:55:36 +0200

    problems

---
+++ Personalsystem/Migrations/201510140754424_problems.cs
@@ -0,0 +1,18 @@
+namespace Personalsystem.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class problems : DbMigration
+    {
+        public override void Up()
+        {
+
+        }
+        
+        public override void Down()
+        {
+
+        }
+    }
+}
---
+++ Personalsystem/Migrations/201510140755074_problems1.cs
@@ -0,0 +1,16 @@
+namespace Personalsystem.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class problems1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
