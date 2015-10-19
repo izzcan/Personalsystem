dd9565daee7908e1407fdfe8291bf89549945e8c
Ambrason <ambrason@gmail.com>
2015-10-19 15:22:57 +0200

    Home viewmodel

--- Personalsystem/Models/HomeViewmodels.cs
+++ Personalsystem/Models/HomeViewmodels.cs
@@ -9,6 +9,15 @@ namespace Personalsystem.Models
     {
         public ICollection<HomeIndexCompany> Companies {get;set;}
         public ICollection<HomeIndexApplication> Applications {get;set;} //Responses / Interviews
+        public ICollection<HomeIndexVacancy> Vacancies { get; set; }
+
+        public HomeIndexViewmodel() { }
+        public HomeIndexViewmodel(ApplicationUser currentUser) 
+        {
+            Companies = currentUser.EmployeeForCompanies.Select(q => new HomeIndexCompany(q, currentUser)).ToList();
+            Applications = currentUser.Applications.Select(q => new HomeIndexApplication(q)).ToList();
+            Vacancies = currentUser.ResponsibleBossForVacancies.Select(q => new HomeIndexVacancy(q)).ToList();
+        }
     }
 
     public class HomeIndexCompany
@@ -30,20 +39,43 @@ namespace Personalsystem.Models
             roles.AddRange(currentUser.BossForDepartments.Where(q => q.Company == that).Select(q => "Boss for " + q.Name).ToList());
             roles.AddRange(currentUser.EmployeeForGroups.Where(q => q.Department.Company == that).Select(q => "Worker in " + q.Department.Name + ":" + q.Name).ToList());
             Roles += string.Join(", ", roles);
-           
         }
     }
 
     //For unemployed
     public class HomeIndexApplication
     {
-    
+        public string CompanyName { get; set; }
+        public string DepartmentName { get; set; }
+        public string PositionName { get; set; }
+        public int NrOfInterviews { get; set; }
+
+        public HomeIndexApplication() { }
+        public HomeIndexApplication(Application that)
+        {
+            this.CompanyName = that.Vacancy.Department.Company.Name;
+            this.DepartmentName = that.Vacancy.Department.Name;
+            this.PositionName = that.Vacancy.Title;
+            this.NrOfInterviews = that.Interviews.Count();
+        }
     }
 
     //For boss
     public class HomeIndexVacancy
     {
-    
+        public string CompanyName { get; set; }
+        public string DepartmentName { get; set; }
+        public string PositionName { get; set; }
+        public int NrOfApplications { get; set; }
+
+        public HomeIndexVacancy() { }
+        public HomeIndexVacancy(Vacancy that) 
+        {
+            this.CompanyName = that.Department.Company.Name;
+            this.DepartmentName = that.Department.Name;
+            this.PositionName = that.Title;
+            this.NrOfApplications = that.Applications.Count();
+        }
     }
 
     //3 latest newsitems per company
--- Personalsystem/Models/IdentityModels.cs
+++ Personalsystem/Models/IdentityModels.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
 
 namespace Personalsystem.Models
 {
@@ -17,6 +18,26 @@ namespace Personalsystem.Models
         public virtual ICollection<Company> LeaderForCompanies { get; set; }
         public virtual ICollection<Department> BossForDepartments { get; set; }
         public virtual ICollection<DepartmentGroup> EmployeeForGroups { get; set; }
+        public virtual ICollection<Application> Applications { get; set; }
+
+        [NotMapped]
+        public virtual ICollection<Company> EmployeeForCompanies
+        {
+            get
+            {
+                var inGroups = EmployeeForGroups.Select(q => q.Department.Company);
+                var bossFor = BossForDepartments.Select(q => q.Company);
+                return AdminForCompanies.Union(LeaderForCompanies).Union(inGroups).Union(bossFor).ToList();
+            }
+        }
+        [NotMapped]
+        public virtual ICollection<Vacancy> ResponsibleBossForVacancies
+        {
+            get
+            {
+                return BossForDepartments.SelectMany(q => q.Vacancies).ToList();
+            }
+        }
 
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
