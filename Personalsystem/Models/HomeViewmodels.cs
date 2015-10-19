7d44167370d8bc77a3f939debed3d680aee85004
Ambrason <ambrason@gmail.com>
2015-10-19 15:39:15 +0200

    Home viewmodel 2

--- Personalsystem/Models/HomeViewmodels.cs
+++ Personalsystem/Models/HomeViewmodels.cs
@@ -8,7 +8,7 @@ namespace Personalsystem.Models
     public class HomeIndexViewmodel
     {
         public ICollection<HomeIndexCompany> Companies {get;set;}
-        public ICollection<HomeIndexApplication> Applications {get;set;} //Responses / Interviews
+        public ICollection<HomeIndexApplication> Applications {get;set;}
         public ICollection<HomeIndexVacancy> Vacancies { get; set; }
 
         public HomeIndexViewmodel() { }
@@ -24,7 +24,7 @@ namespace Personalsystem.Models
     {
         public string Name {get;set;}
         public string Roles {get;set;} //"Admin", "Boss for HR", "Worker in HR :: HR1", "Leader" etc
-        public ICollection<HomeIndexNewsItem> NewsItems {get;set;} //3 latest newsitems per company
+        public ICollection<NewsListitemViewmodel> NewsItems { get; set; } //3 latest newsitems per company
 
         public HomeIndexCompany(){}
         public HomeIndexCompany(Company that, ApplicationUser currentUser)
@@ -39,6 +39,8 @@ namespace Personalsystem.Models
             roles.AddRange(currentUser.BossForDepartments.Where(q => q.Company == that).Select(q => "Boss for " + q.Name).ToList());
             roles.AddRange(currentUser.EmployeeForGroups.Where(q => q.Department.Company == that).Select(q => "Worker in " + q.Department.Name + ":" + q.Name).ToList());
             Roles += string.Join(", ", roles);
+
+            NewsItems = that.NewsItems.OrderByDescending(q => q.Created).Take(3).Select(q => new NewsListitemViewmodel() { Id = q.Id, Title = q.Title, Content = q.Content, Created = q.Created, CreatorName = q.Creator.UserName }).ToList();
         }
     }
 
@@ -78,9 +80,9 @@ namespace Personalsystem.Models
         }
     }
 
-    //3 latest newsitems per company
-    public class HomeIndexNewsItem
-    {
+    ////3 latest newsitems per company
+    //public class HomeIndexNewsItem
+    //{
     
-    }
+    //}
 }
